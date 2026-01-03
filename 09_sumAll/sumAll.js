d0c559eb254790503a6c5497171989016018aa66
xjken <63294438+xjken@users.noreply.github.com>
2026-01-04 02:22:19 +0800

    Finsihed exercise

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if( num1<0 || num2<0 || 
+        !Number.isInteger(num1) || !Number.isInteger(num2))
+        return "ERROR"
+    if(num1 > num2){
+        [num1, num2] = [num2, num1]
+    }
+    let sum = 0;
+    for(let i = num1; i <= num2 ; i++){
+        sum+=i;
+    }
+    return sum;
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
