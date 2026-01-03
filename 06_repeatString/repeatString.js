5a1eb64ae5bdca7c685b981063cb197ae59c6f08
xjken <63294438+xjken@users.noreply.github.com>
2026-01-04 01:42:32 +0800

    Finished exercise

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    if(num<0){
+        return "ERROR";
+    }
+    let newStr = ""
+    for(let i=0;i<num;i++){
+        newStr=newStr+str;
+    }
+    return newStr;
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
