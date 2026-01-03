5c1821ad4f0b50b2431101da6cee58aafd576f69
xjken <63294438+xjken@users.noreply.github.com>
2026-01-04 02:46:50 +0800

    Finished exercise

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  //step 1 subtract by 32
+  let celcius = fahrenheit - 32;
+  //step 2 
+  return parseFloat((celcius*5/9).toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celcius) {
+  let fahrenheit = parseFloat((celcius*9/5).toFixed(1));
+  return fahrenheit+32;
 };
 
 // Do not edit below this line
