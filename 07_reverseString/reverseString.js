bacbfd2796baec2311944b7c4c51774d23a9d6ac
xjken <63294438+xjken@users.noreply.github.com>
2026-01-04 01:51:05 +0800

    finished exercise

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let reversedStr = "";
+    for(let i= str.length -1 ; i>=0;i--){
+        reversedStr = reversedStr+str[i];
+    }
+    return reversedStr;
 };
 
 // Do not edit below this line
