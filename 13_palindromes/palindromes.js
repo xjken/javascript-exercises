34c276419f7c6d3e86e94570baf322627153d9f8
xjken <63294438+xjken@users.noreply.github.com>
2026-01-07 17:57:28 +0800

    finished exercise

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,20 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
+    const cleanStr = str
+                        .toLowerCase()
+                        .split('')
+                        .filter((char)=>alphanumerical.includes(char))
+                        .join('')
+                        
+    const reversedCleanStr = cleanStr
+                                .split('')
+                                .reverse()
+                                .join('')
 
+    return cleanStr === reversedCleanStr
 };
 
+// console.log(palindromes("Racecar!"))
+// console.log(palindromes('ZZZZ car, a man, a maracaz.'))
 // Do not edit below this line
 module.exports = palindromes;
