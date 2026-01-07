3d4ee9dc938cb6583fc9353b57b9c304d41b14ff
xjken <63294438+xjken@users.noreply.github.com>
2026-01-07 18:34:05 +0800

    finished exercise

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,26 @@
-const fibonacci = function() {
+const fibonacci = function(countArg) {
+    //accept strings
+    let count;
+    if(typeof countArg != Number){
+        count = parseInt(countArg);
+    } else{
+        count = countArg;
+    }
 
+    //does not accept negative
+    if(count<0) return "OOPS";
+
+    if(count === 0) return 0;
+
+    let firstPrev = 1; // index 0
+    let secondsPrev = 1; // index 1
+
+    for(let i = 2 ; i<count ;i++){
+        let current = firstPrev + secondsPrev;
+        firstPrev = secondsPrev;
+        secondsPrev = current;
+    }
+    return secondsPrev;
 };
 
 // Do not edit below this line
